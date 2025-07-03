bbe2cc6723c833d8f0ad938ffa0ad87028582f55
omniyanasser <https://github.com/omniyanasser>
2025-07-03 03:31:25 +0300

    Add task-22

---
+++ Frontend/TailwindCss/Task-22/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      borderRadius: {
+        '5xl': '3rem',
+      },
+    },
+  },
+};
